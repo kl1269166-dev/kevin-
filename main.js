fca7c6e369ae324f27cf2e5a03a82930c7a1c62a
kl1269166-dev <kl1269166@gmail.com>
2026-04-28 22:43:35 -0300

    página tailwind productos (js)

---
+++ main.js
@@ -0,0 +1,18 @@
+//{const nombre = prompt("Ingrese su nombre");
+//if (nombre.length < 3) {
+//alert("Ingrese bien su nombre");
+//} //else if (nombre.length >= 3) {
+//const apellido = prompt("Ingrese su apellido");
+//if (apellido.length < 3) {
+//alert("Ingrese bien su apellido");
+//} else if (apellido.length >= 3) {
+//const email = prompt("Ingrese su email");
+
+// const edad = prompt("Ingrese su edad");
+// if (edad >= 18 && edad <= 120) {
+//  alert("Podes entrar");
+// } else if (edad < 18 || edad > 120) {
+//  alert("No podes entrar");
+// }
+// }
+//}
